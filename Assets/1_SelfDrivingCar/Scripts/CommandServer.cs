89e69978ea6d555942748b9b730995789acb6e3d
slaweksystem <slaweksystem@gmail.com>
2025-05-29 20:28:26 +0200

    trudne sie wylosowało

--- Assets/1_SelfDrivingCar/Scripts/CommandServer.cs
+++ Assets/1_SelfDrivingCar/Scripts/CommandServer.cs
@@ -17,6 +17,7 @@ public class CommandServer : MonoBehaviour
 	// Use this for initialization
 	void Start()
 	{
+		Debug.Log("CommandServer: Start");
 		_socket = GameObject.Find("SocketIO").GetComponent<SocketIOComponent>();
 		_socket.On("open", OnOpen);
 		_socket.On("steer", OnSteer);
@@ -38,45 +39,51 @@ public class CommandServer : MonoBehaviour
 	// 
 	void onManual(SocketIOEvent obj)
 	{
-		Debug.Log("ONMANUAL KOZIOŁ");
+		//Debug.Log("CommandServer: Manual");
 		EmitTelemetry (obj);
 	}
 	
 	void OnSteer(SocketIOEvent obj)
 	{
-		Debug.Log("HUJ KOZIOŁ");
+		//Debug.Log("CommandServer: OnSteer");
         JSONObject jsonObject = obj.data;
-		print("OTrzymalem fajnego jsona: " + jsonObject);
+		//print("OTrzymalem fajnego jsona: " + jsonObject);
 		// print("Output Steering: " + ;
-		print("Output throttle: " + float.Parse(jsonObject.GetField("throttle").str, CultureInfo.InvariantCulture.NumberFormat));
+		//print("Output throttle: " + float.Parse(jsonObject.GetField("throttle").str, CultureInfo.InvariantCulture.NumberFormat));
 		CarRemoteControl.SteeringAngle = float.Parse(jsonObject.GetField("steering_angle").str, CultureInfo.InvariantCulture.NumberFormat);
 		CarRemoteControl.Acceleration = float.Parse(jsonObject.GetField("throttle").str, CultureInfo.InvariantCulture.NumberFormat);
 		CarRemoteControl.Brake = float.Parse(jsonObject.GetField("handbrake").str, CultureInfo.InvariantCulture.NumberFormat);
 
-		print("Takie mam parametry : " + CarRemoteControl.SteeringAngle);
+		//print("Takie mam parametry : " + CarRemoteControl.SteeringAngle);
 		EmitTelemetry(obj);
 	}
 
 
 	void EmitTelemetry(SocketIOEvent obj)
 	{
-		 UnityMainThreadDispatcher.Instance().Enqueue(() =>
+		print("CommandServer: EmitTelemetry - Sending");
+		UnityMainThreadDispatcher.Instance().Enqueue(() =>
 		{
-			print("Działam dobrze");
+			
 			// send only if it's not being manually driven
-			if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.S))) {
+			if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.S)))
+			{
+				//print("CommandServer: EmitTelemetry - Really Sending");
 				_socket.Emit("telemetry", new JSONObject());
 			}
-			else {
+			else
+			{
 				// Collect Data from the Car
+				//print("CommandServer: EmitTelemetry - Really Sending");
 				Dictionary<string, string> data = new Dictionary<string, string>();
 				data["steering_angle"] = _carController.CurrentSteerAngle.ToString("N4");
 				data["throttle"] = _carController.AccelInput.ToString("N4");
 				data["speed"] = _carController.CurrentSpeed.ToString("N4");
 				data["image"] = Convert.ToBase64String(CameraHelper.CaptureFrame(FrontFacingCamera));
-				_socket.Emit("telemetry", new JSONObject(data));				
+				_socket.Emit("telemetry", new JSONObject(data));
 			}
 		});
+		//Debug.Log("CommandServer: EmitTelemetry - Something sent");
 
 		//    UnityMainThreadDispatcher.Instance().Enqueue(() =>
 		//    {
